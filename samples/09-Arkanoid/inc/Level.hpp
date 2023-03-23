0f09d9758bde365fd1ba43721030019a85d240f5
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-03-23 16:59:29 +0100

    Update level.

--- samples/09-Arkanoid/inc/Level.hpp
+++ samples/09-Arkanoid/inc/Level.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 #include "Brick.hpp"
+#include "Physics.hpp"
 
+#include <Graphics/Image.hpp>
+#include <Math/Circle.hpp>
+
+#include <optional>
 #include <vector>
 
 class Level
@@ -11,6 +16,17 @@ public:
 
     explicit Level( int levelId );
 
-private:
+    void update( float deltaTime );
+    void draw( Graphics::Image& image );
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="c"></param>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    std::optional<Physics::HitInfo> checkCollision( const Math::Circle& c, const glm::vec2& v ) const;
+
+private:
+    std::vector<Brick> bricks;
 };
