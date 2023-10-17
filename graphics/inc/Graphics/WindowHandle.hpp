d91b8926b0c5cbffe908c82e58b8dd379ec7c6c9
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-10-17 14:24:21 +0200

    Update solution folder for GLFW.

--- graphics/inc/Graphics/WindowHandle.hpp
+++ graphics/inc/Graphics/WindowHandle.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #if defined( GLFW )
-struct GLFWwindow; // Forward declare GLFWwindow struct to avoid including glfw.h
+struct GLFWwindow;  // Forward declare GLFWwindow struct to avoid including glfw.h
 #elif defined( _WIN32 )
 struct HWND__;  // Forward declare the HWND__ struct to avoid including Windows.h.
 #endif
 
 namespace Graphics
 {
-#if defined(GLFW)
+#if defined( GLFW )
 using WindowHandle = GLFWwindow*;
 #elif defined( _WIN32 )
 using WindowHandle = HWND__*;
